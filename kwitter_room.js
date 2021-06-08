8e24f7ae49d8c9f5902aaf6b6c03adaf83771a52
GalaxyVinita <75704799+GalaxyVinita@users.noreply.github.com>
2021-06-08 14:45:32 -0500

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -10,40 +10,48 @@ var firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
 
 
-user_name = localStorage.getItem("username");
-document.getElementById("usname").innerHTML = "Welcome " + user_name + "!";
+  user_name = localStorage.getItem("username");
+  document.getElementById("usname").innerHTML = "Welcome " + user_name + "!";
+
+function addRoom()
+{
+    room_name = document.getElementById("room_name").value;
+
+    firebase.database().ref("/").child(room_name).update({
+          purpose: "adding room name"
+    });
+
+    localStorage.setItem("room_name", room_name);
+
+    //random html name
+    window.location = "kwitter_page.html"
+}
+
+
 
 function getData() 
 {firebase.database().ref("/").on('value',
- function(snapshot) {document.getElementById("output").innerHTML = "";
- snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       
-      Room_names = childKey;
-      console.log("Room Name" + Room_names);
-      row = "<div class='room_name' id=" + Room_names +
-       "onclick='redirectToRoomName(this.id)'> #" + Room_names + "</div>";
-      document.getElementById("output").innerHTML += row;
+function(snapshot) {document.getElementById("output").innerHTML = "";
+snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
      
-      });});}
+    Room_names = childKey;
+    console.log("Room Name" + Room_names);
+    row = "<div class='room_name' id=" + Room_names +
+     "onclick='redirectToRoomName(this.id)'> #" + Room_names + "</div><hr>";
+    document.getElementById("output").innerHTML += row;
+    //End code
+    });});}
 getData();
 function redirectToRoomName(x)
 {
-      console.log(x);
-      localStorage.setItem("room_name", x);
-      window.location = "kwitter_page.html";
+    console.log(x);
+    localStorage.setItem("room_name", x);
+    window.location = "kwitter_page.html";
 }
 
-
-// testing stuff (enter function)
-window.addEventListener("keydown", my_keydown);
-function my_keydown(e){
-    var keypressed = e.keyCode;
-    console.log(keypressed);
-
-    if(keypressed == '13'){
-      enter(); 
-    }
-}
-function enter(){
-    addUser()
+function logout()
+{
+    localStorage.removeItem("username");
+    localStorage.removeItem("room_name");
+    window.location = "index.html";
 }
